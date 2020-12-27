4b3c578b2787ed05521410c2222ca8b05412b906
Nadav Mozes <numad@live.com>
2020-12-27 06:58:39 +0200

    mail details and compose feature

--- Mail/service/mail-service.js
+++ Mail/service/mail-service.js
@@ -25,9 +25,9 @@ function _createMails() {
 
 function _getDemoMails() {
     const mails = [
-        { id: utilsService.makeId(), type: 'income', address: 'Nadav@gmail.com', subject: 'Hello all!', body: `Hi and welcome to our mail app`, isStarred: true, isRead: false, sentAt: 1659833930320 },
-        { id: utilsService.makeId(), type: 'outcome', address: 'Adina@gmail.com', subject: 'Check it out!', body: 'Check my Keep app', isStarred: true, isRead: false, sentAt: 1551155690578 },
-        { id: utilsService.makeId(), type: 'draft', address: 'Alon@gmail.com', subject: 'lets do it!', body: 'Pick up!', isStarred: false, isRead: false, sentAt: 1551133930594 },
+        { id: utilsService.makeId(), type: 'income', address: 'Nadav@gmail.com', subject: 'Hello all!', body: `Hi and welcome to our mail app, our app provides many great features, go ahead try it out!`, isStarred: true, isRead: false, sentAt: 1659833930320 },
+        { id: utilsService.makeId(), type: 'outcome', address: 'Adina@gmail.com', subject: 'Check it out!', body: 'Check my Keep app, this app make you save your Keeps and share it with your friends and family', isStarred: true, isRead: false, sentAt: 1551155690578 },
+        { id: utilsService.makeId(), type: 'draft', address: 'Alon@gmail.com', subject: 'lets do it!', body: 'Text Summary is an application created as an efficient tool to summarize texts automatically and quickly, which will select the most relevant information from your books or texts and will allow you to optimize your time.', isStarred: false, isRead: false, sentAt: 1551133930594 },
     ]
     return mails
 }
